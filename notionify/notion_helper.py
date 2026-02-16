4677e099faa58498cbe19c47c02c557856772110
Ulysses <51317695+edwardcuchulainn@users.noreply.github.com>
2026-02-16 13:38:59 +0800

    Rename query method to query_database

--- notionify/notion_helper.py
+++ notionify/notion_helper.py
@@ -84,7 +84,7 @@ class NotionHelper:
         has_more = True
         start_cursor = None
         while has_more:
-            response = self.client.databases.query(
+            response = self.client.databases.query_database(
                 database_id=database_id,
                 start_cursor=start_cursor,
                 page_size=100,
